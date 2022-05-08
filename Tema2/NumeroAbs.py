3934c35bc412438699f6f350473f0ebecebd67a3
Luis Donaldo López Martínez <105167486+LuisDonaldo2003@users.noreply.github.com>
2022-05-08 13:25:32 -0500

    Create NumeroAbs.py

---
+++ Tema2/NumeroAbs.py
@@ -0,0 +1,17 @@
+from abc import ABC
+
+class NumeroAbs(ABC):
+
+    @staticmethod
+    def Cantidad(cifra):
+        totalDigito = 0
+        if cifra == 0 or cifra < 10:
+            totalDigito = 1
+        elif cifra == 10 or cifra < 100:
+            totalDigito = 2
+        elif cifra == 100 or cifra < 1000:
+            totalDigito = 3
+        elif cifra == 1000 or cifra < 10000:
+            totalDigito = 4
+
+        return totalDigito
